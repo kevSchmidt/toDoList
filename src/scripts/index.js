ac8a2284691f10f3e6c9b7b0c4029009368b8f81
Kevin <kev.saintigny.schmidt@gmail.com>
2020-04-22 17:09:06 +0200

    responsive ?

---
+++ src/scripts/index.js
@@ -0,0 +1,22 @@
+function colorGenerator() {
+  let randNum = Math.ceil(Math.random() * 180);
+  let randNum2 = Math.ceil(Math.random() * 205);
+  let randNum3 = Math.ceil(Math.random() * 225);
+  return `rgba(${randNum},${randNum2},${randNum3},0.6)`;
+}
+
+const add = () => {
+  let userInput = document.querySelector(".user-input").value;
+  let newItem = document.createElement("li");
+  let textLine = document.createTextNode(userInput);
+  newItem.appendChild(textLine);
+  document.querySelector(".list-item").appendChild(newItem);
+  document.querySelector(".user-input").value = "";
+
+  let list = document.querySelectorAll("li");
+  list.forEach((listItem) => {
+    listItem.style.backgroundColor = colorGenerator();
+    listItem.style.listStyle = "none";
+    listItem.style.color = "#fff";
+  });
+};
