1397f9c9cd8a8f37a249d88f7994e544f8c8776a
Kevin <kev.saintigny.schmidt@gmail.com>
2020-04-23 10:50:44 +0200

    add icon

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,8 +1,8 @@
 function colorGenerator() {
-  let randNum = Math.ceil(Math.random() * 180);
-  let randNum2 = Math.ceil(Math.random() * 205);
-  let randNum3 = Math.ceil(Math.random() * 225);
-  return `rgba(${randNum},${randNum2},${randNum3},0.6)`;
+  let randNum = Math.ceil(Math.random() * 230);
+  let randNum2 = Math.ceil(Math.random() * 225);
+  let randNum3 = Math.ceil(Math.random() * 220);
+  return `rgba(${randNum},${randNum2},${randNum3},0.8)`;
 }
 
 const add = () => {
@@ -14,9 +14,23 @@ const add = () => {
   document.querySelector(".user-input").value = "";
 
   let list = document.querySelectorAll("li");
-  list.forEach((listItem) => {
-    listItem.style.backgroundColor = colorGenerator();
-    listItem.style.listStyle = "none";
-    listItem.style.color = "#fff";
+  list.forEach(({ style: listItem }) => {
+    listItem.backgroundColor = colorGenerator();
+    listItem.listStyle = "none";
+    listItem.color = "#fff";
+    listItem.fontWeight = "700";
+    listItem.textShadow = "0 0.1rem 0.3rem black";
+    listItem.paddingLeft = ".8rem";
+    listItem.borderRadius = "25px";
+  });
+
+  let span = document.createElement("span");
+  let text = document.createElement("i");
+  text.className = "fas fa-times-circle";
+  span.className = "close";
+  span.appendChild(text);
+  list.forEach((item) => {
+    item.appendChild(span);
   });
 };
+// on click display:none
